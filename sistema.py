089da5a043c0ec924ae42144de12f3ffa7648aa8
FabricioVenturim <fabriciodalviventurim@gmail.com>
2022-07-07 18:36:15 -0300

    Adicione o print_matriz

--- sistema.py
+++ sistema.py
@@ -7,6 +7,7 @@ def resolve_sistema(matriz, variaveis):
         matriz (array): matriz formada a partir das equações
         variaveis (lista): lista com as variáveis presentes nas equações passadas
     """    
+    print_matriz(matriz, variaveis)
 
     #### Zera lado esquerdo ####
     linha_index = 0
@@ -18,12 +19,14 @@ def resolve_sistema(matriz, variaveis):
             linha_pivo = matriz[coluna] * matriz[linha_index, coluna] 
             #Substitui a linha da matriz pela nova linha escalonada
             matriz[linha_index] = matriz[linha_index] - linha_pivo
+            print_matriz(matriz, variaveis)
         #Colocando o primeiro valor não zero da linha igual a 1
         if matriz[linha_index, linha_index] != 0:
-            matriz[linha_index] = matriz[linha_index]/matriz[linha_index, linha_index] 
-        linha_index += 1 
+            matriz[linha_index] = matriz[linha_index]/matriz[linha_index, linha_index]
+            print_matriz(matriz, variaveis)
+        linha_index += 1
+
     linha_index -= 1
-    
     #### Analisar a quantidade de soluções do sistema
     if matriz[linha_index, linha_index] == 0 and matriz[linha_index, linha_index + 1] == 0:
         print("O sistema de equações possui infinitas soluções")
@@ -39,10 +42,34 @@ def resolve_sistema(matriz, variaveis):
                 linha_pivo = matriz[coluna] * matriz[linha_index, coluna]
                 #Substitui a linha da matriz pela nova linha escalonada
                 matriz[linha_index] = matriz[linha_index] - linha_pivo
+                print_matriz(matriz, variaveis)
             linha_index -= 1
             
         matriz_final = np.around(matriz, 2)
+        print("\n\n")
         for variavel in variaveis:
             index = variaveis.index(variavel)
-            print(f"\nO valor da variável {variavel} é: {matriz_final[index][-1]}")
+            print(f"O valor da variável {variavel} é: {matriz_final[index][-1]}")
 
+def print_matriz(matriz, variaveis):
+    """print matriz de forma mais elegante
+
+    Args:
+        matriz (array): matriz formada a partir das equações
+        variaveis (lista): lista com as variáveis presentes nas equações passadas
+    """    
+    print("\n\n")
+    print(" "*4, end="")
+    for variavel in variaveis:
+            print(variavel, end=" "*5)
+    for linha in matriz:
+        print("")
+        print("[ ", end="")
+        contador = 0
+        for item in linha:
+            if contador == len(linha)-1:
+                print("| ", end="")
+            text = str(item.round(2))
+            print(text.center(5), end=" ")
+            contador += 1
+        print("]", end="")
