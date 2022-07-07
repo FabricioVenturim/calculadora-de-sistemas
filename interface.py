0c24967ac6ea5886eb081263034bcd9f525900d3
FabricioVenturim <fabriciodalviventurim@gmail.com>
2022-07-07 14:17:31 -0300

    Finalize a parte funcional do programa

--- interface.py
+++ interface.py
@@ -2,20 +2,22 @@ import numpy as np
 import sistema
 
 def interface():
-    quantidade = verifica_int("Quantas equações têm seu sitema? ")
+    quantidade_de_equacoes = verifica_int("Quantas equações têm seu sitema? ")
     equacoes = []
     variaveis = []
-    multiplos = []
 
-    for num_equacao in range(1, quantidade + 1):
+    for num_equacao in range(1, quantidade_de_equacoes + 1):
         equacao = input(f"Digite a {num_equacao}ª equação: ")    
         equacoes.append(equacao)
         encontra_variaveis(equacao, variaveis)
 
-    encontra_multiplos(equacoes,variaveis, multiplos)
-    print(multiplos)
-    matriz = np.array(multiplos, dtype="double")
-    sistema.resolve_sistema(matriz, variaveis)
+    #Verificar se a quantidade de variaveis é igual a quantidade de equações
+    if len(variaveis) != quantidade_de_equacoes:
+        print("Desculpe, só consigo trabalhar, ainda, com quantidades iguais de variáveis e equações")
+    else:
+        multiplos = encontra_multiplos(equacoes,variaveis)
+        matriz = np.array(multiplos, dtype="double")
+        sistema.resolve_sistema(matriz, variaveis)
 
 def verifica_int(msg):
     while True:
@@ -31,15 +33,15 @@ def verifica_int(msg):
     return inteiro
 
 
-def encontra_variaveis(equacao, variaveis_list):
+def encontra_variaveis(equacao, variaveis):
     caracteres = list(equacao)
     for caractere in caracteres:
         if caractere.isalpha():
-            if not caractere in variaveis_list:
-                variaveis_list.append(caractere)
+            if not caractere in variaveis:
+                variaveis.append(caractere)
 
-
-def encontra_multiplos(equacoes, variaveis_list, multiplos_list):
+def encontra_multiplos(equacoes, variaveis_list):
+    multiplos_list = []
     for equacao in equacoes:
         partes = equacao.split()
         numeros_da_equacao = []
@@ -67,3 +69,4 @@ def encontra_multiplos(equacoes, variaveis_list, multiplos_list):
                 numeros_da_equacao.append(constante)
 
         multiplos_list.append(numeros_da_equacao)
+    return multiplos_list
--- sistema.py
+++ sistema.py
@@ -1,14 +1,6 @@
 import numpy as np
 
 def resolve_sistema(matriz, variaveis):
-    print(matriz)
-    zera_colunas(matriz, variaveis)
-    matriz_final = np.around(matriz, 2)
-    for variavel in variaveis:
-        index = variaveis.index(variavel)
-        print(f"O valor da variável {variavel} é: {matriz_final[index][-1]}")
-
-def zera_colunas(matriz, variaveis):
     #### Zera lado esquerdo ####
     linha_index = 0
     #passa linha por linha
@@ -19,20 +11,30 @@ def zera_colunas(matriz, variaveis):
             linha_pivo = matriz[coluna] * matriz[linha_index, coluna] 
             #Substitui a linha da matriz pela nova linha escalonada
             matriz[linha_index] = matriz[linha_index] - linha_pivo
-            print(matriz)
         #Colocando o primeiro valor não zero da linha igual a 1
-        matriz[linha_index] = matriz[linha_index]/matriz[linha_index, linha_index] 
-        print(matriz)
+        if matriz[linha_index, linha_index] != 0:
+            matriz[linha_index] = matriz[linha_index]/matriz[linha_index, linha_index] 
         linha_index += 1 
+    linha_index -= 1
+    #### Analisar a quantidade de soluções do sistema
+    if matriz[linha_index, linha_index] == 0 and matriz[linha_index, linha_index + 1] == 0:
+        print("O sistema de equações possui infinitas soluções")
+    elif matriz[linha_index, linha_index] == 0:
+        print("O sistema de equações é impossível")
+    else: 
+        #### Zera lado direio ####
+        #Agora vamos escalonar de baixo para cima
+        # linha_index = len(matriz) - 1
+        while linha_index >= 0:
+            for coluna in range(len(variaveis) - 1, linha_index, -1):
+                #em cada linha, passa coluna por coluna até o 1 da matriz identidade 
+                linha_pivo = matriz[coluna] * matriz[linha_index, coluna]
+                #Substitui a linha da matriz pela nova linha escalonada
+                matriz[linha_index] = matriz[linha_index] - linha_pivo
+            linha_index -= 1
+            
+        matriz_final = np.around(matriz, 2)
+        for variavel in variaveis:
+            index = variaveis.index(variavel)
+            print(f"\nO valor da variável {variavel} é: {matriz_final[index][-1]}")
 
-    #### Zera lado direio ####
-    #Agora vamos escalonar de baixo para cima
-    # linha_index = len(matriz) - 1
-    while linha_index >= 0:
-        for coluna in range(len(variaveis) - 1, linha_index, -1):
-            #em cada linha, passa coluna por coluna até o 1 da matriz identidade 
-            linha_pivo = matriz[coluna] * matriz[linha_index, coluna]
-            #Substitui a linha da matriz pela nova linha escalonada
-            matriz[linha_index] = matriz[linha_index] - linha_pivo
-            print(matriz)
-        linha_index -= 1
